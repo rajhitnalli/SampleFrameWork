930b0535edafdb494fa2a85f52ee173fba47da06
rajhitnalli <rajhitnalli@gmail.com>
2023-03-15 16:15:35 +0530

    1st

---
+++ src/test/java/Practice/Sample1.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	@Test
+	public void m1()
+	{
+		System.out.println("sample1 executed");
+		//from git for pull
+		//from  eclipes for 2nd push
+		//masn as
+	}
+}
